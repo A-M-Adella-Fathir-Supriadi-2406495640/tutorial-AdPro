03dfb101c37d0faedad32de5bb630db74f5992ee
Raithser <m.adella@ui.ac.id>
2026-03-10 19:49:45 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,12 +8,26 @@ public class Payment {
     private String status;
     private Map<String, String> paymentData;
 
-    public Payment(String id, String method, Map<String, String> paymentData) {}
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {}
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "WAITING_CONFIRMATION";
+    }
+
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
+    }
 
     public String getId() { return id; }
     public String getMethod() { return method; }
     public String getStatus() { return status; }
     public Map<String, String> getPaymentData() { return paymentData; }
-    public void setStatus(String status) {}
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
