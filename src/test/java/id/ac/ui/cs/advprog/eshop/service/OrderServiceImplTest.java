4eb2b5cff3c1bff9dde45c72a7da6d16f057c372
Raithser <m.adella@ui.ac.id>
2026-03-10 17:51:50 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class OrderServiceTest {
+class OrderServiceImplTest {
 
     @InjectMocks
     OrderServiceImpl orderService;
@@ -149,4 +149,4 @@ class OrderServiceTest {
         assertTrue(results.isEmpty());
     }
 }
-}
+
